445f29b66f325aeda450ee2e989e402bf7f78b47
sahilagni66 <145852964+sahilagni66@users.noreply.github.com>
2024-01-20 17:51:25 +0530

    Update Sticker.js

--- src/commands/utils/Sticker.js
+++ src/commands/utils/Sticker.js
@@ -9,7 +9,7 @@ export default class Command extends BaseCommand {
             aliases: ['s'],
             description: {
                 content: 'Convert Images/Videos into Stickers',
-                usage: '[packname] | [authorname]'
+                usage: '[𝐅𝖾ᥣ𝗂𝗑] | [𝐓ɦ𝖾 𝐃αᑯᑯ𝗒]'
             },
             dm: true,
             exp: 8
